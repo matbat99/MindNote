7f2874ef47e27b9128c6b864a843d63e1e585e39
Zachary Kwiecinski <Zachary.Kwiecinski@gmail.com>
2019-05-27 19:58:03 +0200

    disabled auth key verification on list auto populate

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,6 @@
 class SongsController < ApplicationController
+  protect_from_forgery except: :create # disabled forgery protection until we can figure out how to do this in JS
+
   def create
     raise
   end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,36 +1,11 @@
 import "bootstrap";
+import { searchItunes } from '../plugins/init_itunes_search';
 
-
-const resultList = document.querySelector('#song-search-results')
 
 const searchBox = document.querySelector("#song-search");
 
-const baseUrl = "https://itunes.apple.com/search?term="
 
-
-const outputToScreen = (songs) => {
-  resultList.innerHTML = "";
-  if (songs) {
-    songs.forEach((song) => {
-      resultList.insertAdjacentHTML('beforeend',
-        `<form action="/songs" method='post'> <input type="hidden" name="album_url" value='${song['collectionViewUrl']}'>
-        <input type="text" name="title" value='${song['trackName']}'>
-        <input type="text" name="artist" value='${song['artistName']}'>
-        <input type="submit">`
-        )
-    })
-  }
-}
-
-
-searchBox.addEventListener('submit', (event) => {
+searchBox.addEventListener('keyup', (event) => {
   event.preventDefault();
-  const searchTerm= event.currentTarget.querySelector('.query');
-
-  fetch(baseUrl+searchTerm.value+"&entity=musicTrack&limit=5")
-  .then(response => response.json())
-  .then((data)=> {
-    console.log(data);
-    outputToScreen(data.results);
-  });
+  searchItunes();
 });
