6e72a9f68b3adc362b71526c91b8901d0c639065
Matthew Bates <matbat99@yahoo.com>
2019-05-27 18:33:00 +0200

    search api working song creation not working

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -1,8 +1,10 @@
 class PracticesController < ApplicationController
+  require 'open-uri'
   def index
   end
 
   def create
+
   end
 
   def show
@@ -10,7 +12,19 @@ class PracticesController < ApplicationController
 
   def destroy
   end
-  
+
   def learn
+  end
+
+  def song_search
+    search_term = params[:query]
+    @songs = []
+    response = open("https://itunes.apple.com/search?term=#{search_term}&entity=musicTrack&limit=5").read
+    json = JSON.parse(response)
+    json['results'].each do |song|
+      @songs << {title: song['trackName'], album_url: song['collectionViewUrl'],)
+    end
+    raise
+    render '/pages/home'
   end
 end
