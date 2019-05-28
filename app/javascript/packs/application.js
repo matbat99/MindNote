076bc810e1fb5f0630f3f3f229fa8a4e5d6de786
Matthew Bates <matbat99@yahoo.com>
2019-05-28 14:46:13 +0200

    protect autocomplete when form not present

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,9 @@ import { searchItunes } from '../plugins/init_itunes_search';
 
 const searchBox = document.querySelector("#song-search");
 
-
+if(searchBox) {
 searchBox.addEventListener('keyup', (event) => {
   event.preventDefault();
   searchItunes();
 });
+};
