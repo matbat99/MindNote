3c9d17884a110a1721d820eb34643f878067c688
Matthew Bates <matbat99@yahoo.com>
2019-05-28 12:30:32 +0200

    song create form autocomplete and modal

--- app/javascript/plugins/init_itunes_search.js
+++ app/javascript/plugins/init_itunes_search.js
@@ -1,15 +1,15 @@
 const outputToScreen = (songs) => {
-  const resultList = document.querySelector('#song-search-results')
-  resultList.innerHTML = "";
+  const resultList = document.querySelector('.new_song')
+  resultList.innerHTML = '<input type="submit" name="commit" value="add a song" data-disable-with="add a song">'
   if (songs) {
     songs.forEach((song) => {
-      resultList.insertAdjacentHTML('beforeend',
-        `<form action="/songs" method='post'>
+      resultList.insertAdjacentHTML('afterbegin',
+        `  <div class="form-group">
           <input type="hidden" name="album_url" value='${song['collectionViewUrl']}'>
           <input type="text" name="title" value='${song['trackName']}'>
           <input type="text" name="artist" value='${song['artistName']}'>
-          <input type="submit">
-        </form>`
+          <input type="radio" name="choice" id="choice" value="1">
+           </div>`
         )
     })
   }
@@ -27,3 +27,5 @@ const searchItunes =()=> {
 }
 
 export { searchItunes }
+
+
