e5239157879d97159696725d7e439d494f4a7c61
Matthew Bates <matbat99@yahoo.com>
2019-05-29 11:33:24 +0200

    fix urgent tasks syntax

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -17,9 +17,9 @@ class Practice < ApplicationRecord
   def self.urgent_songs(user_id)
     user = User.find(user_id)
     practices = user.practices
-    practices.select { |practice| practice.active? }
-    practices.select { |practice| practice.need_to_practice?}
-    practices.each { |practice| practice.update_importance }
+    practices = practices.select { |practice| practice.active? }
+    practices = practices.select { |practice| practice.need_to_practice?}
+    practices = practices.each { |practice| practice.update_importance }
     practices.sort_by { |practice| practice.importance }
     practices.reverse
   end
