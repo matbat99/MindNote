5a55a355b753be77204192a70b76f3bcf505891f
Matthew Bates <matbat99@yahoo.com>
2019-05-29 14:13:03 +0200

    view songs to review

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   def home
     @song = Song.new
     @unlearned = current_user.practices.where(active: false)
+    @songs_due = Practice.urgent_songs(current_user.id).count
   end
 end
