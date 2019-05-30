95afff098e06aedffd70456ffa97b241a3b1391e
Matthew Bates <matbat99@yahoo.com>
2019-05-30 16:51:27 +0200

    move modals to views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @song = Song.new
     @unlearned = current_user.practices.where(active: false)
     @songs_due = Practice.urgent_songs(current_user.id).count
   end
