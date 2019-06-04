ab5cbeb6a17f5aa0be5e4533dd8753f61c9f52ad
Zachary Kwiecinski <Zachary.Kwiecinski@gmail.com>
2019-06-04 18:17:43 +0200

    integrate import song option into navbar

---
+++ app/controllers/playlists_controller.rb
@@ -0,0 +1,31 @@
+class PlaylistsController < ApplicationController
+  def index
+  end
+
+  def show
+    playlist_id = params[:playlist_id]
+    access_token = params[:access_token]
+    url = "https://api.spotify.com/v1/playlists/#{playlist_id}/tracks"
+    playlist_data = JSON.parse(open(url, "Authorization" => "Bearer #{access_token}").read)
+    @songs = playlist_data["items"]
+  end
+
+  def create
+    if params[:songs].nil?
+      redirect_to root_path
+    else
+      data = params[:songs]
+      @songs = data.map { |song| JSON.parse(song) }
+      # raise
+      @songs.each do |song_data|
+        artist = Artist.find_or_create_by(name: song_data["artist"])
+        song = Song.find_or_create_by(title: song_data["name"], artist: artist, album_url: song_data["url"])
+        practice = Practice.create!(user: current_user, song: song)
+      end
+      redirect_to :root
+    end
+  rescue JSON::ParserError => e
+    flash.notice = "Sorry something went wrong, please try again"
+    redirect_to :root
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
   resources :songs, only: [:new, :create]
   get 'songs/learn', to: "songs#learn", as: :learn
 
+  # resources :learn_songs, only: :update
+
   resources :practices, only: [:index, :show, :destroy] do
     resources :sessions, only: [:new, :create]
     resources :notes, only: [:create, :index, :show, :destroy]
     collection do
       get "review"
-    end
-    collection do
       get "progress"
     end
     member do
@@ -19,4 +19,6 @@ Rails.application.routes.draw do
     end
     resources :sessions, only: [:create]
   end
+
+  resources :playlists, only: [:index, :show, :create]
 end
